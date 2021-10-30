62be1e6261fb507bd699f2d4a3dff7b17c5fab14
Aditi Sawant <90927378+Sparkle000@users.noreply.github.com>
2021-10-30 16:03:58 +0530

    fibonacci

---
+++ Fibonacci.cpp
@@ -0,0 +1,17 @@
+//n th element in fibonacci series
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int fib(int n)
+{
+    return n<2?n:fib(n-1) + fib(n-2);
+}
+ 
+int main ()
+{
+    int n;
+    cin>>n;
+    cout << fib(n);
+    
+    return 0;
+}
